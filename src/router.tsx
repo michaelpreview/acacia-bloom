8c709fc2e2cce5a9cc0c277a05337d9241371f9a
Michael Begisen <michaelmshindi@gmail.com>
2026-06-06 13:12:19 +0300

    Fixxxx

--- src/router.tsx
+++ src/router.tsx
@@ -2,18 +2,25 @@ import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
+// 1. Define router context type (IMPORTANT)
 export interface RouterContext {
   queryClient: QueryClient;
 }
 
-export const queryClient = new QueryClient();
+export const getRouter = () => {
+  const queryClient = new QueryClient();
 
-export const router = createRouter({
-  routeTree,
-  basepath: '/acacia/',
-  context: {
-    queryClient,
-  },
-  scrollRestoration: true,
-  defaultPreloadStaleTime: 0,
-});
+  const router = createRouter({
+    routeTree,
+
+    // 2. Provide context
+    context: {
+      queryClient,
+    },
+
+    scrollRestoration: true,
+    defaultPreloadStaleTime: 0,
+  });
+
+  return router;
+};
