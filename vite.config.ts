3598d6e88c68ccea36d744145bd4e9cc86a509be
Michael Begisen <michaelmshindi@gmail.com>
2026-06-06 12:46:54 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/acacia/',
+  base: '/acacia-bloom/',
 
   plugins: [
     react(),
